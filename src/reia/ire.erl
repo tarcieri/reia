101db2fe8b31a5a2d4aa22f63054ceec856e9698
Tony Arcieri <tony@medioh.com>
2008-05-13 15:28:01 -0600

    Initial pretty printer for ire

--- src/reia/ire.erl
+++ src/reia/ire.erl
@@ -26,9 +26,37 @@ eval_print(String, Binding) ->
   {value, Value, NewBinding} = reia_eval:exprs(Parsed, Binding),
   print(Value),
   NewBinding.
+
+print(Value) -> io:format("~s~n", [stringify_term(Value)]).
+
+stringify_term(Term) when is_integer(Term) -> stringify_integer(Term);
+stringify_term(Term) when is_float(Term)   -> stringify_float(Term);
+stringify_term(Term) when is_list(Term)    -> stringify_list(Term);
+stringify_term(Term) when is_tuple(Term)   -> stringify_compound(Term).
+
+stringify_integer(Int) -> integer_to_list(Int).
+stringify_float(Float) ->
+  [String|_] = io_lib:format("~f", [Float]),
+  String.
+
+stringify_list(List) -> "[" ++ lists:concat(stringify_list_members(List, [])) ++ "]".
+
+stringify_list_members([], Acc) -> lists:reverse(Acc);
+stringify_list_members([Term|Rest], Acc) ->
+  NewAcc = if 
+    Rest == [] -> [Term|Acc];
+    true       -> [",",Term|Acc]
+  end,
+  stringify_list_members(Rest, NewAcc).
   
-print(Result) -> 
-  io:format("~p~n", [Result]).
+stringify_compound(Term = {Type, Data}) ->
+  case Type of
+    string -> stringify_string(Term);
+    regexp -> stringify_regexp(Term)
+  end.
+  
+stringify_string({string, Binary}) -> "\"" ++ binary_to_list(Binary) ++ "\"".
+stringify_regexp({regexp, Binary}) -> "/" ++ binary_to_list(Binary) ++ "/".
   
 print_error(Error) ->
   io:format("Error: ~p~n", [Error]).
