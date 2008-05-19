4840e1d8b31d07cd2de89cdb7f04f7d9d640569a
Tony Arcieri <tony@medioh.com>
2008-05-19 03:21:24 -0600

    Support for List#push

--- src/reia/reia_lists.erl
+++ src/reia/reia_lists.erl
@@ -1,8 +1,23 @@
 -module(reia_lists).
 -export([funcall/3]).
 
+%% List#reverse
 funcall({list, {Elements, Order}}, reverse, []) ->
   case Order of
     normal  -> {list, {Elements, reverse}};
     reverse -> {list, {Elements, normal}}
-  end.  
+  end;
+  
+%% List#push
+funcall({list, {Elements, Order}}, push, Values) ->
+  case Order of
+    normal ->
+      push(lists:reverse(Elements), Values);
+    reverse ->
+      push(Elements, Values)
+  end.
+  
+push(Elements, []) ->
+  {list, {Elements, reverse}};
+push(Elements, [Value|Rest]) ->
+  push([Value|Elements], Rest).
