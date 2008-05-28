c897fba13688c66730eedb61f0e0376bef0468af
Tony Arcieri <tony@medioh.com>
2008-05-28 15:48:54 -0600

    Implement String#to_list

--- src/reia/reia_string.erl
+++ src/reia/reia_string.erl
@@ -2,4 +2,6 @@
 -export([funcall/3]).
 
 funcall({string, String}, to_s, []) ->
-  {string, list_to_binary("\"" ++ binary_to_list(String) ++ "\"")}.
+  {string, list_to_binary("\"" ++ binary_to_list(String) ++ "\"")};
+funcall({string, String}, to_list, []) ->
+  reia_erl:e2r(binary_to_list(String)).
