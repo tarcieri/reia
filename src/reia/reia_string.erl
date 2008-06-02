4555edccd131d7a9b3b630d79054dc4a579565a5
Tony Arcieri <tony@medioh.com>
2008-06-02 03:02:07 -0600

    Implement String#to_upper and String#to_lower

--- src/reia/reia_string.erl
+++ src/reia/reia_string.erl
@@ -1,7 +1,24 @@
 -module(reia_string).
 -export([funcall/3]).
 
+%% String#to_s
+%%   Generate a string representation of a string
 funcall({string, String}, to_s, []) ->
   {string, list_to_binary("\"" ++ binary_to_list(String) ++ "\"")};
+  
+%% String#to_list
+%%   Cast a string explicitly to a list
 funcall({string, String}, to_list, []) ->
-  reia_erl:e2r(binary_to_list(String)).
+  reia_erl:e2r(binary_to_list(String));
+  
+%% String#to_upper
+%%   Capitalize all letters string
+funcall({string, String}, to_upper, []) ->
+  NewString = string:to_upper(binary_to_list(String)),
+  reia_list:funcall(reia_erl:e2r(NewString), to_string, []);
+  
+%% String#to_lower
+%%   Remove capitalization from a string
+funcall({string, String}, to_lower, []) ->
+  NewString = string:to_lower(binary_to_list(String)),
+  reia_list:funcall(reia_erl:e2r(NewString), to_string, []).
