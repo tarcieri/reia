70d75f53c1cd6514eb294b9dbab372fff52d23c2
Tony Arcieri <tony@medioh.com>
2008-06-01 19:42:22 -0600

    Convert reia_tuple to Reia

--- src/reia/reia_dispatch.erl
+++ src/reia/reia_dispatch.erl
@@ -9,7 +9,7 @@ funcall(Receiver, Method, Arguments) when is_atom(Receiver) ->
 funcall(Receiver = {list, _}, Method, Arguments) ->
   reia_list:funcall(Receiver, Method, Arguments);
 funcall(Receiver = {tuple, _}, Method, Arguments) ->
-  reia_tuple:funcall(Receiver, Method, Arguments);
+  reia_tuple:funcall(Receiver, Method, silly_list_hack(Arguments));
 funcall(Receiver = {dict, _}, Method, Arguments) ->
   reia_dict:funcall(Receiver, Method, Arguments);
 funcall(Receiver = {string, _}, Method, Arguments) ->
@@ -17,9 +17,13 @@ funcall(Receiver = {string, _}, Method, Arguments) ->
 funcall(Receiver = {lambda, _}, Method, Arguments) ->
   reia_lambda:funcall(Receiver, Method, Arguments);
 funcall(Receiver = {regexp, _}, Method, Arguments) ->
-  reia_regexp:funcall(Receiver, Method, reia_erl:e2r(Arguments)).
+  reia_regexp:funcall(Receiver, Method, silly_list_hack(Arguments)).
   
 %% Funcalls that take blocks
 funcall(Receiver = {list, _}, Method, Arguments, Block) ->
   reia_list:funcall(Receiver, Method, Arguments, Block).
   
+%% Reia doesn't have proper pattern matching for lists yet, so use a temporary hack
+%% when talking to modules implemented directly in Reia
+silly_list_hack(List) ->
+  {list, {List, normal}}.
--- src/reia/reia_tuple.erl
+++
@@ -1,6 +0,0 @@
--module(reia_tuple).
--export([funcall/3]).
-
-funcall({tuple, Members}, to_s, []) ->
-  String = "(" ++ reia_list:funcall(reia_erl:e2r(erlang:tuple_to_list(Members)), join, [","]) ++ ")",
-  reia_list:funcall(reia_erl:e2r(String), to_string, []).
