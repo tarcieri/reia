c99d6a2ffbbc5c5dd16836b3010faaa1a65f2dbe
Tony Arcieri <tony@medioh.com>
2008-06-28 15:16:12 -0600

    Implement String#capitalize

--- src/reia/reia_string.erl
+++ src/reia/reia_string.erl
@@ -11,14 +11,21 @@ funcall({string, String}, to_s, []) ->
 funcall({string, String}, to_list, []) ->
   reia_erl:e2r(binary_to_list(String));
   
-%% String#downcase
+%% String#upcase
 %%   Capitalize all letters string
-funcall({string, String}, downcase, []) ->
+funcall({string, String}, upcase, []) ->
   NewString = string:to_upper(binary_to_list(String)),
   reia_list:funcall(reia_erl:e2r(NewString), to_string, []);
   
-%% String#upcase
+%% String#downcase
 %%   Remove capitalization from a string
-funcall({string, String}, upcase, []) ->
+funcall({string, String}, downcase, []) ->
   NewString = string:to_lower(binary_to_list(String)),
+  reia_list:funcall(reia_erl:e2r(NewString), to_string, []);
+  
+%% String#capitalize
+%%   Capitalize the first letter of a string, downcasing all others
+funcall({string, String}, capitalize, []) ->
+  [FirstLetter|Rest] = binary_to_list(String),
+  NewString = string:to_upper([FirstLetter]) ++ string:to_lower(Rest),
   reia_list:funcall(reia_erl:e2r(NewString), to_string, []).
