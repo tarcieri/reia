b21060a5c3eaf842334945aa066a304641ef0eca
Tony Arcieri <tony@medioh.com>
2008-08-26 01:49:13 -0600

    Walk constants, identifiers, and strings

--- src/reia/compiler/reia_visitor.erl
+++ src/reia/compiler/reia_visitor.erl
@@ -34,6 +34,12 @@ ast({funcall, Line, Module, Name, Expressions}, Fun) ->
     ast(Name, Fun), 
     lists:flatten([ast(Expression, Fun) || Expression <- Expressions])
   });
+ast(Node = {constant, _Line, _Name}, Fun) ->
+  Fun(Node);
+ast(Node = {identifier, _Line, _Name}, Fun) ->
+  Fun(Node);
+ast(Node = {string, _Line, _Value}, Fun) ->
+  Fun(Node);  
 ast(Ast, _Fun) ->
   io:format("warning: unrecognized AST node: ~p~n", [Ast]),
   Ast.
