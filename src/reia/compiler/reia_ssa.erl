6a60e7cbed74df0f82840bf314f49a6c3fa9e2b7
Tony Arcieri <tony@medioh.com>
2008-09-03 02:25:38 -0600

    New state-holding visitor

--- src/reia/compiler/reia_ssa.erl
+++ src/reia/compiler/reia_ssa.erl
@@ -1,7 +1,9 @@
 -module(reia_ssa).
--export([ast/1]).
+-export([ast/1, transform/2]).
 
 ast(Ast) ->
-  reia_visitor:transform(Ast, fun(Node) ->
-    Node
-  end).
+  {ok, placeholder, Ast2} = reia_visitor:transform(Ast, placeholder, fun reia_ssa:transform/2),
+  Ast2.
+  
+transform(State, Node) ->
+  {walk, State, Node}.
--- src/reia/compiler/reia_visitor.erl
+++ src/reia/compiler/reia_visitor.erl
@@ -1,45 +1,48 @@
 -module(reia_visitor).
--export([transform/2]).
+-export([transform/3, transformer/2]).
 
-transform(Expressions, Fun) ->
-  lists:flatten([ast(Expression, Fun) || Expression <- Expressions]).
+transform(Expressions, State, Fun) when is_list(Expressions) ->
+  {Expressions2, {State2, _Fun}} = lists:mapfoldl(fun reia_visitor:transformer/2, {State, Fun}, Expressions),
+  {ok, State2, Expressions2};
+  
+transform(Node, State, Fun) ->
+  case Fun(State, Node) of
+    {walk, State2, Node2} ->
+      walk(Node2, State2, Fun);
+    {stop, State2, Node2} ->
+      {ok, State2, Node2};
+    Value ->
+      throw({error, {"invalid_transform result", Value}})
+  end.
+  
+transformer(Node, {State, Fun}) ->
+  {ok, State2, Node2} = transform(Node, State, Fun),
+  {Node2, {State2, Fun}}.
 
-ast({module, Line, Name, Expressions}, Fun) ->
-  Fun({
-    module, 
-    Line, 
-    ast(Name, Fun), 
-    lists:flatten([ast(Expression, Fun) || Expression <- Expressions])
-  });
-ast({function, Line, Name, Arguments, Expressions}, Fun) ->
-  Fun({
-    function,
-    Line,
-    ast(Name, Fun),
-    lists:flatten([ast(Argument, Fun) || Argument <- Arguments]),
-    lists:flatten([ast(Expression, Fun) || Expression <- Expressions])
-  });
-ast({funcall, Line, Name, Expressions}, Fun) ->
-  Fun({
-    funcall,
-    Line,
-    ast(Name, Fun),
-    lists:flatten([ast(Expression, Fun) || Expression <- Expressions])
-  });
-ast({funcall, Line, Module, Name, Expressions}, Fun) ->
-  Fun({
-    funcall, 
-    Line, 
-    ast(Module, Fun), 
-    ast(Name, Fun), 
-    lists:flatten([ast(Expression, Fun) || Expression <- Expressions])
-  });
-ast(Node = {constant, _Line, _Name}, Fun) ->
-  Fun(Node);
-ast(Node = {identifier, _Line, _Name}, Fun) ->
-  Fun(Node);
-ast(Node = {string, _Line, _Value}, Fun) ->
-  Fun(Node);  
-ast(Ast, _Fun) ->
+walk({module, Line, Name, Expressions}, State, Fun) ->
+  {ok, State2, Name2} = transform(Name, State, Fun),
+  {ok, State3, Expressions2} = transform(Expressions, State2, Fun),
+  {ok, State3, {module, Line, Name2, Expressions2}};
+walk({function, Line, Name, Arguments, Expressions}, State, Fun) ->
+  {ok, State2, Name2} = transform(Name, State, Fun),
+  {ok, State3, Arguments2} = transform(Arguments, State2, Fun),
+  {ok, State4, Expressions2} = transform(Expressions, State3, Fun),  
+  {ok, State4, {function, Line, Name2, Arguments2, Expressions2}};
+walk({funcall, Line, Name, Expressions}, State, Fun) ->
+  {ok, State2, Name2} = transform(Name, State, Fun),
+  {ok, State3, Expressions2} = transform(Expressions, State2, Fun),
+  {ok, State3, {funcall, Line, Name2, Expressions2}};
+walk({funcall, Line, Module, Name, Expressions}, State, Fun) ->
+  {ok, State2, Module2} = transform(Module, State, Fun),
+  {ok, State3, Name2} = transform(Name, State2, Fun),
+  {ok, State4, Expressions2} = transform(Expressions, State3, Fun),
+  {ok, State4, {funcall, Line, Module2, Name2, Expressions2}};
+walk({constant, _Line, _Name} = Node, State, _Fun) ->
+  {ok, State, Node};
+walk({identifier, _Line, _Name} = Node, State, _Fun) ->
+  {ok, State, Node};
+walk({string, _Line, _Value} = Node, State, _Fun) ->
+  {ok, State, Node};
+walk(Ast, State, _Fun) ->
   % io:format("warning: unrecognized AST node: ~p~n", [Ast]),
-  Ast.
+  {ok, State, Ast}.
