b1605e5c90cec51bebeec36e92b7d345b7cafddc
Tony Arcieri <tony@medioh.com>
2008-09-08 02:03:43 -0600

    SSA transform support for function nodes

--- src/reia/compiler/reia_ssa.erl
+++ src/reia/compiler/reia_ssa.erl
@@ -9,8 +9,15 @@
 -export([ast/1, transform/2]).
 
 ast(Ast) ->
-  {ok, placeholder, Ast2} = reia_visitor:transform(Ast, placeholder, fun reia_ssa:transform/2),
+  {ok, _, Ast2} = reia_visitor:transform(Ast, dict:new(), fun reia_ssa:transform/2),
   Ast2.
   
-transform(State, Node) ->
-  {walk, State, Node}.
+transform(Dict, {function, Line, Name, Arguments, Expressions}) ->
+  {ok, Dict2, Arguments2} = transform_list(Arguments, dict:new()),
+  {ok, _, Expressions2} = transform_list(Expressions, Dict2),
+  {stop, Dict, {function, Line, Name, Arguments2, Expressions2}};
+transform(Dict, Node) ->
+  {walk, Dict, Node}.
+  
+transform_list(List, State) ->
+  reia_visitor:transform(List, State, fun transform/2).
