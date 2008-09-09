55286a6dd4d095991313b41e7eea0a3ec9b1e1e5
Tony Arcieri <tony@medioh.com>
2008-09-09 00:51:52 -0600

    Refactor with mode tracking

--- src/reia/compiler/reia_ssa.erl
+++ src/reia/compiler/reia_ssa.erl
@@ -9,23 +9,34 @@
 -export([ast/1, transform/2]).
 
 ast(Ast) ->
-  {ok, _, Ast2} = reia_visitor:transform(Ast, dict:new(), fun reia_ssa:transform/2),
+  {ok, _, Ast2} = reia_visitor:transform(Ast, {normal, dict:new()}, fun reia_ssa:transform/2),
   Ast2.
   
-transform(Dict, {function, Line, Name, Arguments, Expressions}) ->
-  {ok, Dict2, Arguments2} = reia_visitor:transform(Arguments, dict:new(), fun transform/2),
-  {ok, _, Expressions2} = reia_visitor:transform(Expressions, Dict2, fun transform/2),
-  {stop, Dict, {function, Line, Name, Arguments2, Expressions2}};
-transform(Dict, {match, Line, In1, In2}) ->
-  {ok, Dict2, Out1} = reia_visitor:transform(In1, Dict, fun transform_match/2),
-  {ok, Dict3, Out2} = reia_visitor:transform(In2, Dict2, fun transform/2),
-  {stop, Dict3, {match, Line, Out1, Out2}};
-transform(Dict, Node) ->
-  {walk, Dict, Node}.  
+% Function declarations create a new scope
+transform(State, {function, Line, Name, Arguments, Expressions}) ->
+  % Create a new scope with dict:new()
+  {ok, Dict, Arguments2} = reia_visitor:transform(Arguments, {normal, dict:new()}, fun transform/2),
+  {ok, _, Expressions2} = reia_visitor:transform(Expressions, Dict, fun transform/2),
+    
+  % Return to the original scope
+  {stop, State, {function, Line, Name, Arguments2, Expressions2}};
   
-transform_match(Dict, {identifier, _Line, _Name} = Arg) ->
-  {stop, Dict, Arg};
-transform_match(Dict, Node) ->
-  {walk, Dict, Node}.
-      
+% Match expressions mutate variables on the LHS
+transform({Mode, Dict}, {match, Line, In1, In2}) ->
+  {ok, {_, Dict2}, Out1} = reia_visitor:transform(In1, {match, Dict}, fun transform/2),
+  {ok, {_, Dict3}, Out2} = reia_visitor:transform(In2, {Mode, Dict2}, fun transform/2),
+  {stop, {Mode, Dict3}, {match, Line, Out1, Out2}};
+  
+% Normally identifiers are mapped to their latest version
+transform({normal, _} = State, {identifier, _Line, _Name} = Node) ->
+  {walk, State, Node};
+  
+% On the LHS of match expressions, variables are assigned a new version
+transform({match, Dict}, {identifier, _Line, _Name} = Node) ->
+  io:format("Match expression: ~p~n", [Node]),
+  {stop, {match, Dict}, Node};
+  
+% Walk unrecognized nodes without transforming them
+transform(State, Node) ->
+  {walk, State, Node}.      
   
