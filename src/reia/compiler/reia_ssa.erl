93a652183631f29ae21b94be4711d303e4645eae
Tony Arcieri <tony@medioh.com>
2008-09-09 00:37:45 -0600

    Begin separating out identifiers in match expressions

--- src/reia/compiler/reia_ssa.erl
+++ src/reia/compiler/reia_ssa.erl
@@ -13,11 +13,19 @@ ast(Ast) ->
   Ast2.
   
 transform(Dict, {function, Line, Name, Arguments, Expressions}) ->
-  {ok, Dict2, Arguments2} = transform_list(Arguments, dict:new()),
-  {ok, _, Expressions2} = transform_list(Expressions, Dict2),
+  {ok, Dict2, Arguments2} = reia_visitor:transform(Arguments, dict:new(), fun transform/2),
+  {ok, _, Expressions2} = reia_visitor:transform(Expressions, Dict2, fun transform/2),
   {stop, Dict, {function, Line, Name, Arguments2, Expressions2}};
+transform(Dict, {match, Line, In1, In2}) ->
+  {ok, Dict2, Out1} = reia_visitor:transform(In1, Dict, fun transform_match/2),
+  {ok, Dict3, Out2} = reia_visitor:transform(In2, Dict2, fun transform/2),
+  {stop, Dict3, {match, Line, Out1, Out2}};
 transform(Dict, Node) ->
-  {walk, Dict, Node}.
+  {walk, Dict, Node}.  
   
-transform_list(List, State) ->
-  reia_visitor:transform(List, State, fun transform/2).
+transform_match(Dict, {identifier, _Line, _Name} = Arg) ->
+  {stop, Dict, Arg};
+transform_match(Dict, Node) ->
+  {walk, Dict, Node}.
+      
+  
