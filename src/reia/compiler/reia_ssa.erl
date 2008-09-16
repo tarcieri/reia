e3b84b2a0d0c14098a7005393617a4019cc9ad30
Tony Arcieri <tony@medioh.com>
2008-09-15 19:05:30 -0600

    Add SSA support for case statements, funs, and functions that take blocks

--- src/reia/compiler/reia_ssa.erl
+++ src/reia/compiler/reia_ssa.erl
@@ -6,16 +6,30 @@
 %
 
 -module(reia_ssa).
--export([ast/1, transform/2]).
+-export([ast/1, ast/2, transform/2]).
 
 %-define(msg(Str, Xs), io:format(Str, Xs)).
 -define(msg(Str, Xs), ok).
 
 ast(Ast) ->
+  ast(Ast, []).
+  
+ast(Ast, Variables) ->
+  Dict = dict:from_list([{Variable, 0} || Variable <- Variables]),
   ?msg("Input: ~p~n", [Ast]),
-  {ok, _, Ast2} = reia_visitor:transform(Ast, {normal, dict:new()}, fun reia_ssa:transform/2),
+  {ok, _, Ast2} = reia_visitor:transform(Ast, {normal, Dict}, fun transform/2),
   ?msg("Output: ~p~n", [Ast2]),
   Ast2.
+
+% Module declarations create a new scope
+transform(State, {module, Line, Name, Expressions}) ->
+  Expressions2 = lists:map(fun(Expression) ->
+      {ok, _, Expression2} = reia_visitor:transform(Expression, {normal, dict:new()}, fun transform/2),
+      Expression2
+    end,
+    Expressions
+  ),
+  {stop, State, {module, Line, Name, Expressions2}};
   
 % Function declarations create a new scope
 transform(State, {function, Line, Name, Arguments, Expressions}) ->
@@ -25,6 +39,12 @@ transform(State, {function, Line, Name, Arguments, Expressions}) ->
     
   % Return to the original scope
   {stop, State, {function, Line, Name, Arguments2, Expressions2}};
+
+% Lambdas close over the outer scope but still bind arguments
+transform({Mode, Dict}, {lambda, Line, Arguments, Expressions}) ->
+  {ok, {_, Dict2}, Arguments2} = reia_visitor:transform(Arguments, {argument, Dict}, fun transform/2),
+  {ok, {_, Dict3}, Expressions2} = reia_visitor:transform(Expressions, {normal, Dict2}, fun transform/2),
+  {stop, {Mode, Dict3}, {lambda, Line, Arguments2, Expressions2}};
   
 % Function names are identifiers and should remain undisturbed
 transform({Mode, _Dict} = State, {funcall, Line, Name, Arguments}) ->
@@ -38,16 +58,17 @@ transform({Mode, _Dict} = State, {funcall, Line, Receiver, Name, Arguments}) ->
   Node = {funcall, Line, Receiver2, Name, Arguments2},
   {stop, {Mode, Dict3}, Node};
 
+transform({Mode, _Dict} = State, {funcall, Line, Receiver, Name, Arguments, Block}) ->
+  {ok, {_, Dict2}, Receiver2}  = reia_visitor:transform(Receiver,  State, fun transform/2), 
+  {ok, {_, Dict3}, Arguments2} = reia_visitor:transform(Arguments, {Mode, Dict2}, fun transform/2),
+  {ok, {_, Dict4}, Block2} = reia_visitor:transform(Block, {Mode, Dict3}, fun transform/2),
+  Node = {funcall, Line, Receiver2, Name, Arguments2, Block2},
+  {stop, {Mode, Dict4}, Node};
+
 transform({Mode, _Dict} = State, {erl_funcall, Line, Module, Function, Arguments}) ->
   {ok, {_, Dict2}, Arguments2} = reia_visitor:transform(Arguments, State, fun transform/2),
   Node = {erl_funcall, Line, Module, Function, Arguments2},
   {stop, {Mode, Dict2}, Node};
-  
-% Match expressions mutate variables on the LHS
-transform({Mode, Dict}, {match, Line, In1, In2}) ->
-  {ok, {_, Dict2}, Out2} = reia_visitor:transform(In2, {Mode, Dict}, fun transform/2),
-  {ok, {_, Dict3}, Out1} = reia_visitor:transform(In1, {match, Dict2}, fun transform/2),
-  {stop, {Mode, Dict3}, {match, Line, Out1, Out2}};
   
 % Arguments should initialize new entries in the SSA dict
 transform({argument, Dict}, {identifier, Line, Name}) ->
@@ -59,7 +80,25 @@ transform({argument, Dict}, {identifier, Line, Name}) ->
       Node = {identifier, Line, ssa_name(Name, 0)},
       {stop, {argument, Dict2}, Node}
   end;
-  
+
+% Match expressions mutate variables on the LHS
+transform({Mode, Dict}, {match, Line, In1, In2}) ->
+  {ok, {_, Dict2}, Out2} = reia_visitor:transform(In2, {Mode, Dict}, fun transform/2),
+  {ok, {_, Dict3}, Out1} = reia_visitor:transform(In1, {match, Dict2}, fun transform/2),
+  {stop, {Mode, Dict3}, {match, Line, Out1, Out2}};
+
+% Case expressions bind variables in clauses
+transform({Mode, Dict}, {'case', Line, Expression, Clauses}) ->
+  {ok, {_, Dict2}, Expression2} = reia_visitor:transform(Expression, {Mode, Dict}, fun transform/2),
+  {ok, {_, Dict3}, Clauses2} = reia_visitor:transform(Clauses, {'case', Dict2}, fun transform/2),
+  {stop, {Mode, Dict3}, {'case', Line, Expression2, Clauses2}};
+
+% Case clauses match against patterns
+transform({'case', Dict}, {clause, Line, Pattern, Expressions}) ->
+  {ok, {_, Dict2}, Pattern2} = reia_visitor:transform(Pattern, {match, Dict}, fun transform/2),
+  {ok, {_, Dict3}, Expressions2} = reia_visitor:transform(Expressions, {normal, Dict2}, fun transform/2),
+  {stop, {'case', Dict3}, {clause, Line, Pattern2, Expressions2}};
+
 % Normally identifiers are mapped to their latest version
 transform({normal, Dict} = State, {identifier, Line, Name}) ->
   case dict:find(Name, Dict) of
