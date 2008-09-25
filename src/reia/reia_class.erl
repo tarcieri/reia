74efdb280ad5ad9bd402cf2ffb76f9f236ef0c08
Tony Arcieri <tony@medioh.com>
2008-09-24 20:30:14 -0600

    Implement full gen_server behavior

--- src/reia/reia_class.erl
+++ src/reia/reia_class.erl
@@ -73,7 +73,11 @@ default_functions() ->
   [{Name, parse_function(String)} || {Name, String} <- [
     {init,           "init(Args) -> initialize(Args), {ok, dict:new()}."},
     {initialize,     "initialize(_Args) -> void."}, 
-    {method_missing, "method_missing(_State, Method, _Args) -> throw({error, {Method, \"undefined\"}})."}
+    {method_missing, "method_missing(_State, Method, _Args) -> throw({error, {Method, \"undefined\"}})."},
+    {handle_cast,    "handle_cast(_Msg, State) -> {noreply, State}."},
+    {handle_info,    "handle_info(_Info, State) -> {noreply, State}."},
+    {terminate,      "terminate(_Reason, _State) -> ok."},
+    {code_change,    "code_change(_OldVsn, State, _Extra) -> {ok, State}."}
   ]].
   
 % Default methods that Reia objects respond to
