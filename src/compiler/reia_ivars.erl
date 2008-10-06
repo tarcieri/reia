e5119d6e164317b89b08b91906eec132c4fa8fb0
Tony Arcieri <tony@medioh.com>
2008-10-06 01:48:31 -0600

    Detect in-method scope

--- src/compiler/reia_ivars.erl
+++ src/compiler/reia_ivars.erl
@@ -6,21 +6,35 @@
 %
 
 -module(reia_ivars).
--export([ast/1, ast/2, transform/2]).
+-export([ast/1, transform/2]).
 
 %-define(msg(Str, Xs), io:format(Str, Xs)).
 -define(msg(Str, Xs), ok).
 
 ast(Ast) ->
-  ast(Ast, []).
-  
-ast(Ast, Variables) ->
-  Dict = dict:from_list([{Variable, 0} || Variable <- Variables]),
   ?msg("Input: ~p~n", [Ast]),
-  {ok, _, Ast2} = reia_visitor:transform(Ast, {toplevel, Dict}, fun transform/2),
+  {ok, _, Ast2} = reia_visitor:transform(Ast, toplevel, fun transform/2),
   ?msg("Output: ~p~n", [Ast2]),
   Ast2.
 
+transform(State, {class, Line, Name, Expressions}) ->
+  Expressions2 = lists:map(fun(Expression) ->
+      {ok, _, Expression2} = reia_visitor:transform(Expression, class, fun transform/2),
+      Expression2
+    end,
+    Expressions
+  ),
+  {stop, State, {class, Line, Name, Expressions2}};
+  
+transform(class, {function, Line, Name, Arguments, Expressions}) ->
+  Expressions2 = lists:map(fun(Expression) ->
+      {ok, _, Expression2} = reia_visitor:transform(Expression, method, fun transform/2),
+      Expression2
+    end,
+    Expressions
+  ),
+  {stop, class, {function, Line, Name, Arguments, Expressions2}};
+  
 transform(_State, {ivar, Line, _Name}) ->
   throw({error, {Line, "instance variables can only be referenced in classes"}});
       
