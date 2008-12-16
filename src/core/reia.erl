710b2de8a6d7065b202677573e359dd991610929
Tony Arcieri <tony@medioh.com>
2008-12-16 00:39:15 -0700

    Add reia module with initial reia:apply implementation

---
+++ src/core/reia.erl
@@ -0,0 +1,16 @@
+-module(reia).
+-export([apply/3, apply/4]).
+
+% Call the given Reia function from the Erlang environment, automatically 
+% converting methods from Erlang to Reia form.  Automatically assigns the block
+% to nil unless it's explicitly passed otherwise
+apply(Constant, Method, Arguments) ->
+  apply(Constant, Method, Arguments, nil).
+  
+% Call the given Reia function from the Erlang environment, automatically
+% converting methods from Erlang to Reia from.
+% FIXME should disintermediate the block from other formal arguments.
+apply(Constant, Method, Arguments, nil) ->
+  erlang:apply(Constant, Method, [reia_erl:e2r(Arg) || Arg <- Arguments]);
+apply(Constant, Method, Arguments, Block) ->
+  erlang:apply(Constant, Method, [reia_erl:e2r(Arg) || Arg <- Arguments ++ [Block]]).
