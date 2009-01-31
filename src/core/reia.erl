2c000f7220ce1324a18b6b3725421f1c2139d14b
Tony Arcieri <tony@medioh.com>
2009-01-31 15:24:26 -0700

    Add reia:parse() and String#parse() methods

--- src/core/reia.erl
+++ src/core/reia.erl
@@ -1,5 +1,8 @@
 -module(reia).
--export([apply/3, apply/4]).
+-export([
+  apply/3, apply/4,
+  parse/1
+]).
 
 % Call the given Reia function from the Erlang environment, automatically 
 % converting methods from Erlang to Reia form.  Automatically assigns the block
@@ -14,3 +17,9 @@ apply(Constant, Method, Arguments, nil) ->
   erlang:apply(Constant, Method, [reia_erl:e2r(Arg) || Arg <- Arguments]);
 apply(Constant, Method, Arguments, Block) ->
   erlang:apply(Constant, Method, [reia_erl:e2r(Arg) || Arg <- Arguments ++ [Block]]).
+  
+% Parse a given string (in standard Erlang list form) into its Reia parse tree
+%  parse(String) -> {ok, ParseTree} | {error, Reason}
+%  Types  String = list()
+parse(String) ->
+  reia_parse:string(String).
