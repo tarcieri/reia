1e47cc2f719284bfd18ea33289298284d27d72bb
Phil Pirj <pirj@mail.ru>
2009-01-31 18:14:39 +0300

    allow to call meta objects just like this/ object = reia_class::inst(clazz, [], _block)/ result = reia::apply(object, method, [parameter1, parameter2])

--- src/core/reia.erl
+++ src/core/reia.erl
@@ -4,6 +4,8 @@
 % Call the given Reia function from the Erlang environment, automatically 
 % converting methods from Erlang to Reia form.  Automatically assigns the block
 % to nil unless it's explicitly passed otherwise
+apply({object, {Pid, _Module}}, Method, Arguments) ->
+  reia_class:call(Pid, {Method, Arguments});
 apply(Constant, Method, Arguments) ->
   apply(Constant, Method, Arguments, nil).
   
