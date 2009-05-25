c261b05e43f360f29395283afe69d3cd2d29d184
Tony Arcieri <tony@medioh.com>
2009-05-25 15:44:55 -0600

    Correct ** operator to use new call convention

--- src/compiler/reia_operators.erl
+++ src/compiler/reia_operators.erl
@@ -18,7 +18,7 @@ forms('not' = Op, Line, Ast) ->
 
 %% Exponentation
 forms('**', Line, Ast1, Ast2) ->
-  {call, Line, {remote, Line, {atom, Line, 'Numeric'}, {atom, Line, pow}}, [Ast1, Ast2]};
+  {call, Line, {remote, Line, {atom, Line, 'Numeric'}, {atom, Line, pow}}, [{tuple, Line, [Ast1, Ast2]}, {atom, Line, nil}]};
 
 %% Multiplication  
 forms('*' = Op, Line, Ast1, Ast2) ->
