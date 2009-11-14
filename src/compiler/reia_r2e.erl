191fe02b61ae9501665ecb5ef600cb030642e643
Tony Arcieri <tony@medioh.com>
2009-11-14 11:35:10 -0700

    Compiler support for lists

--- src/compiler/reia_r2e.erl
+++ src/compiler/reia_r2e.erl
@@ -12,7 +12,11 @@
 %% Numerical types
 transform(Exprs = #integer{}) -> Exprs;
 transform(Exprs = #float{})   -> Exprs;
-  
+
+%% Lists
+transform(#list{line=Line, elements=Elements}) ->
+  list_cons(Elements, Line);
+
 %% Operators
 transform(#unary_op{line=Line, type=Type, val=Val}) ->
   {op, Line, Type, transform(Val)};
@@ -25,3 +29,9 @@ transform(#binary_op{line=Line, type='**', val1=Val1, val2=Val2}) ->
 
 transform(#binary_op{line=Line, type=Type, val1=Val1, val2=Val2}) ->
   {op, Line, Type, transform(Val1), transform(Val2)}.
+
+%% Generate cons for lists
+list_cons([], Line) ->
+  {nil, Line};
+list_cons([Element|Rest], Line) ->
+  {cons, Line, transform(Element), list_cons(Rest, Line)}.
