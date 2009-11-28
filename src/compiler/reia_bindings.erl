24fc0c4c401264cf1223b911c7193de6c641a4df
Tony Arcieri <tony@medioh.com>
2009-11-28 11:28:31 -0700

    Begin producting Binding Annotated Format through reia_bindings

--- src/compiler/reia_bindings.erl
+++ src/compiler/reia_bindings.erl
@@ -1,44 +1,47 @@
--module(reia_annotator).
--export([annotate/1]).
+-module(reia_bindings).
+-export([transform/1]).
 -include("reia_nodes.hrl").
+-include("reia_bindings.hrl").
 
-% Internal state of the annotator
+% Internal state of the bindings transform
 -record(state, {scope=normal, bindings=dict:new()}).
 
-annotate(Node) ->
+transform(Node) ->
   {Node2, _State} = reia_syntax:mapfold_subtrees(
-    fun annotate/2,
+    fun transform/2,
     #state{},
     Node
   ),
   Node2.
 
 % Walk the LHS of a match expression in match scope
-annotate(#match{} = Node, State) ->
+transform(#match{} = Node, State) ->
   {Right, State2} = reia_syntax:mapfold_subtrees(
-    fun annotate/2,
+    fun transform/2,
     State,
     Node#match.right
   ),
 
   {Left, State3} = reia_syntax:mapfold_subtrees(
-    fun annotate/2,
+    fun transform/2,
     State2#state{scope=match},
     Node#match.left
   ),
 
   Node2  = Node#match{left=Left, right=Right},
   State4 = State3#state{scope=State#state.scope},
-  {Node2, State4};
+  output(Node2, State4);
 
 % Variables are (re)bound while in match scope
-annotate(#identifier{name=Name} = Node, #state{scope=match} = State) ->
+transform(#identifier{name=Name} = Node, #state{scope=match} = State) ->
   NewBindings = case dict:find(Name, State#state.bindings) of
     {ok, Version} -> dict:store(Name, Version + 1);
     error         -> dict:store(Name, 0)
   end,
-  {Node, State#state{bindings=NewBindings}};
+  output(Node, State#state{bindings=NewBindings});
 
-annotate(Node, State) ->
-  io:format("annotating: ~p~n", [Node]),
-  {Node, State}.
+transform(Node, State) ->
+  output(Node, State).
+
+output(Node, State) ->
+  {#bindings{node=Node, entries=State#state.bindings}, State}.
