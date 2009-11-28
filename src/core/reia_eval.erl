1797593df85d6f83c397c58b6c888289ed3f1b0f
Tony Arcieri <tony@medioh.com>
2009-11-28 12:41:33 -0700

    Annotate the return value of eval to include the new bindings

--- src/core/reia_eval.erl
+++ src/core/reia_eval.erl
@@ -28,10 +28,10 @@ string(Str, Bindings) ->
 % Evaluate the given set of expressions
 exprs(Exprs, Bindings) ->
 	io:format("Input Code: ~p~n", [Exprs]),
-	NewBindings = output_bindings(Exprs),
-	io:format("New Bindings: ~p~n", [NewBindings]),
+  Exprs2 = annotate_return_value(Exprs),
+  io:format("Annotated: ~p~n", [Exprs2]),
 
-  {ok, Module} = reia_compiler:compile(temporary_module(), Exprs),
+  {ok, Module} = reia_compiler:compile(temporary_module(), Exprs2),
   {ok, Name, Value} = reia_bytecode:load(Module),
 
 	% FIXME: In the future it's possible eval will create things which persist
@@ -46,8 +46,31 @@ temporary_module() ->
   Nonce = lists:flatten([io_lib:format("~.16b",[N]) || <<N>> <= Hash]),
   "reia_eval#" ++ Nonce.
 
+% Annotate the return value of the expression to include the bindings
+annotate_return_value(Exprs) ->
+  [LastExpr|Exprs2] = lists:reverse(Exprs),
+  lists:reverse([return_value(LastExpr, output_bindings(Exprs))|Exprs2]).
+
 % Obtain a list of all variables which will be bound when eval is complete
 output_bindings(Exprs) ->
   {ok, BAExprs} = reia_bindings:transform(Exprs),
   [#bindings{entries=Entries}|_] = lists:reverse(BAExprs),
   dict:fetch_keys(Entries).
+
+% Generate the return value for eval, appending the binding nodes
+return_value(Expr, Bindings) ->
+  Line = element(2, Expr),
+  {tuple, Line, [
+      Expr,
+      binding_forms([binding_node(Name, Line) || Name <- Bindings], Line)
+  ]}.
+
+% Generate the AST representing a given binding
+binding_node(Name, Line) ->
+  {tuple, Line, [{atom, Line, Name}, {identifier, Line, Name}]}.
+
+% Explode bindings into cons expressions
+binding_forms([], Line) ->
+  {empty, Line};
+binding_forms([Expr|Rest], Line) ->
+  {cons, Line, Expr, binding_forms(Rest, Line)}.
