39a01f1ce02d969e71a8207a48ab4f577b00d701
Tony Arcieri <tony@medioh.com>
2009-11-28 11:30:42 -0700

    Allow the scope given to the bindings annotator to be specifiable

--- src/compiler/reia_bindings.erl
+++ src/compiler/reia_bindings.erl
@@ -1,29 +1,33 @@
 -module(reia_bindings).
--export([transform/1]).
+-export([transform/1, transform/2]).
 -include("reia_nodes.hrl").
 -include("reia_bindings.hrl").
 
-% Internal state of the bindings transform
+% Internal state of the bindings annotator
 -record(state, {scope=normal, bindings=dict:new()}).
 
-transform(Node) ->
+% Annotate the given node with its current bindings
+transform(Node) -> transform(Node, normal).
+
+% Annotate the given node, assuming the given scope
+transform(Node, Scope) ->
   {Node2, _State} = reia_syntax:mapfold_subtrees(
-    fun transform/2,
-    #state{},
+    fun transform_node/2,
+    #state{scope=Scope},
     Node
   ),
   Node2.
 
 % Walk the LHS of a match expression in match scope
-transform(#match{} = Node, State) ->
+transform_node(#match{} = Node, State) ->
   {Right, State2} = reia_syntax:mapfold_subtrees(
-    fun transform/2,
+    fun transform_node/2,
     State,
     Node#match.right
   ),
 
   {Left, State3} = reia_syntax:mapfold_subtrees(
-    fun transform/2,
+    fun transform_node/2,
     State2#state{scope=match},
     Node#match.left
   ),
@@ -33,14 +37,14 @@ transform(#match{} = Node, State) ->
   output(Node2, State4);
 
 % Variables are (re)bound while in match scope
-transform(#identifier{name=Name} = Node, #state{scope=match} = State) ->
+transform_node(#identifier{name=Name} = Node, #state{scope=match} = State) ->
   NewBindings = case dict:find(Name, State#state.bindings) of
     {ok, Version} -> dict:store(Name, Version + 1);
     error         -> dict:store(Name, 0)
   end,
   output(Node, State#state{bindings=NewBindings});
 
-transform(Node, State) ->
+transform_node(Node, State) ->
   output(Node, State).
 
 output(Node, State) ->
