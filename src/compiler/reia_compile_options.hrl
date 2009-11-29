f9512b13454d7d13753e5cd1307b41be3c1a410a
Tony Arcieri <tony@medioh.com>
2009-11-29 11:12:46 -0700

    Disable autohipe by default (no reason to have it on by default right now)

--- src/compiler/reia_compile_options.hrl
+++ src/compiler/reia_compile_options.hrl
@@ -3,7 +3,7 @@
 -record(compile_options, {
   scope         = toplevel,
   passes        = [r2e],
-  autohipe      = true,
+  autohipe      = false,
   toplevel_args = [],
   erlc_options  = [debug_info, export_all, verbose, report_errors]
 }).
