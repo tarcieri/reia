96e2824a4d89bca03e11f94f9b8a5599c79e7590
Tony Arcieri <tony@medioh.com>
2009-11-28 17:11:40 -0700

    Build a compile_options record to store various compiler options

---
+++ src/compiler/reia_compile_options.hrl
@@ -0,0 +1,8 @@
+% Global options for the Reia compiler.  Passed to every single transform.
+% Transforms can use them for what they will.
+-record(compile_options, {
+  scope              = toplevel,
+  passes             = [r2e],
+  toplevel_arguments = [],
+  erlc_options       = [debug_info, export_all, verbose, report_errors]
+}).
--- src/compiler/reia_compiler.erl
+++ src/compiler/reia_compiler.erl
@@ -7,13 +7,39 @@
 
 -module(reia_compiler).
 -export([compile/2, compile/3]).
--define(passes, [r2e]).
+-include("reia_compile_options.hrl").
 
+% Compile the given expressions, which came from the given source filename
 compile(Filename, Expressions) ->
   compile(Filename, Expressions, []).
 
+% Compile the given expressions, which came from the given source filename
+%
+% Accepts a list of compile options, given as 2-tuples.  The compiler
+% recognizes the following option keys in the form {Key, Value}:
+% * scope:              Scope the given expressions are considered to exist in
+% * passes:             Passes which should be applied to the input expressions
+% * toplevel_arguments: A list of arguments the toplevel method accepts
+% * erlc_args:          A list of options to be passed alogn to erlc
 compile(Filename, Expressions, Options) ->
-  Modules = [list_to_atom("reia_" ++ atom_to_list(Pass)) || Pass <- ?passes],
+  OptRecord = lists:foldl(fun process_option/2, #compile_options{}, Options),
+  run_passes(Filename, Expressions, OptRecord).
+
+% Build a compile_options record from the specified options list
+% Since records suck so much, we have to manually specify all of the possible
+% patterns and their behaviors.  Can't DRY it out.  Records suck :(
+process_option({scope, Scope}, Options) ->
+  Options#compile_options{scope = Scope};
+process_option({passes, Passes}, Options) ->
+  Options#compile_options{passes = Passes};
+process_option({toplevel_arguments, Args}, Options) ->
+  Options#compile_options{toplevel_arguments = Args};
+process_option({erlc_options, Opts}, Options) ->
+  Options#compile_options{erlc_options = Opts}.
+
+run_passes(Filename, Expressions, Options) ->
+  Passes = Options#compile_options.passes,
+  Modules = [list_to_atom("reia_" ++ atom_to_list(Pass)) || Pass <- Passes],
   Fun = fun(Pass, Exprs) -> [Pass:transform(Expr) || Expr <- Exprs] end,
   Expressions2 = lists:foldl(Fun, Expressions, Modules),
   reia_bytecode:compile(Filename, Expressions2, Options).
