073bbc84d000dae10bd2afae0f37dca6036871fc
Tony Arcieri <tony@medioh.com>
2009-11-29 20:17:05 -0700

    Initial Tuple builtin methods

---
+++ src/builtins/Tuple.erl
@@ -0,0 +1,8 @@
+-module('Tuple').
+-export([call/4]).
+
+call(Tuple, to_s, _Args, _Block) ->
+  List = tuple_to_list(Tuple),
+  lists:flatten(["(", string:join([convert(Elem) || Elem <- List], ","), ")"]).
+
+convert(Value) -> reia_dispatch:call(Value, to_s, [], nil).
