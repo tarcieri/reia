eca4f3e1fdd012e11686ae2c0f663bc7393e6215
Tony Arcieri <tony@medioh.com>
2009-11-29 19:02:37 -0700

    Dispatch remote calls to the appropriate handler modules

---
+++ src/core/reia_dispatch.erl
@@ -0,0 +1,29 @@
+%
+% reia_dispatch: Dispatch logic for all Reia method invocations
+% Copyright (C)2008 Tony Arcieri
+%
+% Redistribution is permitted under the MIT license.  See LICENSE for details.
+%
+
+-module(reia_dispatch).
+-export([call/4]).
+
+% Dispatch incoming calls
+call(Receiver, Method, Arguments, Block) when is_integer(Receiver) or is_float(Receiver) ->
+  'Numeric':call(Receiver, Method, Arguments, Block);
+call(Receiver, Method, Arguments, Block) when is_atom(Receiver) ->
+  'Atom':call(Receiver, Method, Arguments, Block);
+call(Receiver, Method, Arguments, Block) when is_list(Receiver) ->
+  'List':call(Receiver, Method, Arguments, Block);
+call({dict,_,_,_,_,_,_,_,_} = Receiver, Method, Arguments, Block) ->
+  'Map':call(Receiver, Method, Arguments, Block);
+call(Receiver, Method, Arguments, Block) when is_tuple(Receiver) ->
+  'Tuple':call(Receiver, Method, Arguments, Block);
+call(Receiver, Method, Arguments, Block) when is_binary(Receiver) ->
+  'Binary':call(Receiver, Method, Arguments, Block);
+call(Receiver, Method, Arguments, Block) when is_function(Receiver) ->
+  'Fun':call({Receiver, Method, Arguments}, Block);
+call(Receiver, Method, Arguments, Block) when is_pid(Receiver) ->
+  'Process':call(Receiver, Method, Arguments, Block);
+call(Receiver, _, _, _) ->
+  throw({error, unknown_receiver, Receiver}).
