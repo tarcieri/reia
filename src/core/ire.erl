964388da3e5b6ecc0e7f829a035a3793023066e2
Tony Arcieri <tony@medioh.com>
2009-12-01 00:02:28 -0700

    Wire up ire for basic functionality

--- src/core/ire.erl
+++ src/core/ire.erl
@@ -7,46 +7,66 @@
 
 -module(ire).
 -export([init/0, start/0]).
--include("reia_types.hrl").
 
 init() ->
   io:format("Reia Interactive Shell (prerelease)~nRunning on "),
   user_drv:start('tty_sl -c -e', {ire, start, []}).
 
 start() ->
-  reia:init(),
   spawn(fun() -> loop() end).
 
 loop() ->
   loop(reia_eval:new_binding()).
 
-loop(Binding) ->
-  NewBinding = try
-    parse("", Binding, '>> ')
-  catch
-    Class:Reason -> print_error(Class, Reason),
-    Binding
-  end,
-  loop(NewBinding).
+  loop(Binding) ->
+    case read() of
+      eof -> io:format("~n"); % print a newline then exit
+      String ->
+        NewBinding = try
+          parse(String, Binding)
+        catch
+          Class:Reason -> print_error(Class, Reason),
+          Binding
+        end,
+        loop(NewBinding)
+    end.
+
+read() ->
+  read('>> ').
 
 read(Prompt) ->
   read(standard_io, Prompt).
 
 read(Io, Prompt) ->
   io:get_line(Io, Prompt).
-  
-parse(Input, Binding, Prompt) ->
+
+read_until_complete(Input, Prompt) ->
   Input2 = [read(Prompt)|Input],
-  case reia_parse:string(lists:flatten(lists:reverse(Input2))) of
-    {ok, []} -> %% Need more tokens
-      parse(Input2, Binding, '.. ');
-    {error, {eof, _}} -> %% Need more tokens
-      parse(Input2, Binding, '.. ');
-    {error, Error} ->
+  case reia_parse:parse(lists:flatten(lists:reverse(Input2))) of
+    %% Need more tokens
+    {_Remaining, _Token, _Idx} ->
+      read_until_complete(Input2, Prompt);
+    Result ->
+      Result
+  end.
+
+parse(String, Binding) ->
+  io:format("String is: ~p~n", [String]),
+  case reia_parse:parse(String) of
+    %% Need more tokens
+    {_Remaining, _Token, _Idx} ->
+      case read_until_complete([String], '.. ') of
+        {ok, Exprs} ->
+          eval(Exprs, Binding);
+        {fail, Error} ->
+          parse_error(Error),
+          Binding
+      end;
+    Exprs when is_list(Exprs) ->
+      eval(Exprs, Binding);
+    {fail, Error} ->
       parse_error(Error),
-      Binding;
-    {ok, Exprs} ->
-      eval(Exprs, Binding)    
+      Binding
   end.
 
 eval(Exprs, Binding) ->
@@ -55,14 +75,18 @@ eval(Exprs, Binding) ->
   NewBinding.
 
 print(Value) ->
-  #reia_string{elements=Elements} = reia_dispatch:call(Value, inspect, {}, nil),
-  io:format("=> ~s~n", [iolist_to_binary(Elements)]).
+  io:format("=> ~p~n", [Value]).
+  %{string, String} = reia_dispatch:funcall(Value, inspect, [], nil),
+  %io:format("=> ~s~n", [binary_to_list(String)]).
 
 parse_error({Line, Error}) ->
   io:format("Error: Line ~w: ~s~n", [Line, Error]).
 
-print_error(_Class, #reia_object{} = Object) ->
-	#reia_string{elements=Elements} = reia:invoke(Object, to_s, {}),
-	io:format("~s~n", [iolist_to_binary(Elements)]);
 print_error(Class, Reason) ->
-	reia_internal:print_error(Class, Reason).
+  PF = fun(Term, I) ->
+    io_lib:format("~." ++ integer_to_list(I) ++ "P", [Term, 50])
+  end,
+  StackTrace = erlang:get_stacktrace(),
+  StackFun = fun(M, _F, _A) -> (M =:= erl_eval) or (M =:= ?MODULE) end,
+  Error = lib:format_exception(1, Class, Reason, StackTrace, StackFun, PF),
+  io:format("~s~n", [Error]).
