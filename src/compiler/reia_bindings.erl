740782d31c73d27ebd84f695df175a4587801e8a
Tony Arcieri <tony@medioh.com>
2009-12-06 16:39:35 -0700

    Binding annotations for modules and functions

--- src/compiler/reia_bindings.erl
+++ src/compiler/reia_bindings.erl
@@ -26,6 +26,40 @@ transform(Exprs, Scope) ->
 % Node transformations
 %
 
+% Module declarations create a new scope
+transform_node(#module{functions=Functions} = Node, State) ->
+  {Functions2, _State2} = [
+    reia_syntax:mapfold_subtrees(
+      fun transform_node/2,
+      #state{scope=module},
+      [Function]
+    ) || Function <- Functions
+  ],
+  output(Node#module{functions=Functions2}, State);
+
+% Function declarations create a new scope
+transform_node(#function{line=Line, name=Name, arguments=Args, block=Block, body=Exprs}, State) ->
+  {Args2, #state{bindings=Bindings}} = reia_syntax:mapfold_subtrees(
+    fun transform_node/2,
+    #state{scope=argument},
+    Args
+  ),
+
+  {[Block2], #state{bindings=Bindings2}} = reia_syntax:mapfold_subtrees(
+    fun transform_node/2,
+    #state{scope=argument, bindings=Bindings},
+    [Block]
+  ),
+
+  {Exprs2, _State} = reia_syntax:mapfold_subtrees(
+    fun transform_node/2,
+    #state{scope=normal, bindings=Bindings2},
+    Exprs
+  ),
+
+  Node = #function{line=Line, name=Name, arguments=Args2, block=Block2, body=Exprs2},
+  output(Node, State);
+
 % Walk the LHS of a match expression in match scope
 transform_node(#match{} = Node, State) ->
   {Right, State2} = reia_syntax:mapfold_subtrees(
@@ -43,6 +77,10 @@ transform_node(#match{} = Node, State) ->
   Node2  = Node#match{left=Left, right=Right},
   State4 = State3#state{scope=State#state.scope},
   output(Node2, State4);
+
+% Arguments should initialize new entries in the bindings dict
+transform_node(#identifier{name=Name} = Node, #state{scope=argument, bindings=Bindings} = State) ->
+  output(Node, State#state{bindings=dict:store(Name, 0, Bindings)});
 
 % Variables are (re)bound while in match scope
 transform_node(#identifier{name=Name} = Node, #state{scope=match, bindings=Bindings} = State) ->
