0673f90f67bc7ad35471d1d7b19a1535c7d611d4
Tony Arcieri <tony@medioh.com>
2009-12-07 00:25:31 -0700

    Append version numbers to the ends of variable names.  Hello destructive assignment!

--- src/compiler/reia_ssa.erl
+++ src/compiler/reia_ssa.erl
@@ -9,7 +9,19 @@ transform(Exprs, Options) ->
   {ok, BAExprs} = reia_bindings:transform(Exprs, Options#compile_options.scope),
   reia_syntax:map_subtrees(fun transform_node/1, BAExprs).
 
+transform_node(#bindings{node=#identifier{line=Line, name=Name}, entries=Bindings}) ->
+  case dict:find(Name, Bindings) of
+    {ok, Version} ->
+      #identifier{line=Line, name=ssa_name(Name, Version)};
+    error ->
+      throw({error, {Line, lists:flatten(io_lib:format("unbound variable: '~s'", [Name]))}})
+  end;
 transform_node(#bindings{node=Node}) ->
   reia_syntax:map_subtrees(fun transform_node/1, Node);
 transform_node(Node) ->
   reia_syntax:map_subtrees(fun transform_node/1, Node).
+
+% Generate the SSA name for a given variable, which takes the form name_version
+ssa_name(Name, Version) ->
+  Name2 = lists:flatten(io_lib:format("~s_~w", [Name, Version])),
+  list_to_atom(Name2).
