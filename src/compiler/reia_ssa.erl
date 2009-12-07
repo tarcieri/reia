800a446b67ad01cafe52b8256574aa60c4df3ddf
Tony Arcieri <tony@medioh.com>
2009-12-06 23:58:35 -0700

    Handle reverting from binding annotated form in the SSA transform itself

--- src/compiler/reia_ssa.erl
+++ src/compiler/reia_ssa.erl
@@ -7,4 +7,9 @@
 % Lists of expressions
 transform(Exprs, Options) ->
   {ok, BAExprs} = reia_bindings:transform(Exprs, Options#compile_options.scope),
-  reia_bindings:revert(BAExprs).
+  reia_syntax:map_subtrees(fun transform_node/1, BAExprs).
+
+transform_node(#bindings{node=Node}) ->
+  reia_syntax:map_subtrees(fun transform_node/1, Node);
+transform_node(Node) ->
+  reia_syntax:map_subtrees(fun transform_node/1, Node).
