0d36be3ecb0f5ec3ef260a9b79bb2bd75d49813c
Tony Arcieri <tony@medioh.com>
2009-12-12 17:23:00 -0700

    Support for retrieving members of Maps

--- src/builtins/Map.erl
+++ src/builtins/Map.erl
@@ -1,6 +1,12 @@
 -module('Map').
 -export([call/4]).
 
+call(Map, '[]', {Key}, _Block) ->
+  case dict:find(Key, Map) of
+    {ok, Value} -> Value;
+    error       -> nil
+  end;
+
 call(Map, to_s, _Args, _Block) ->
   List = [
     io_lib:format("~s=>~s", [convert(Key), convert(Value)]) ||
