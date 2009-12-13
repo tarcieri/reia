e82a6e0a23cc671fc135c535e86d8f805d81844c
Tony Arcieri <tony@medioh.com>
2009-12-12 17:25:45 -0700

    Support for retrieving members of tuples

--- src/builtins/Tuple.erl
+++ src/builtins/Tuple.erl
@@ -1,6 +1,9 @@
 -module('Tuple').
 -export([call/4]).
 
+call(Tuple, '[]', {Index}, _Block) ->
+  element(Index + 1, Tuple);
+
 call(Tuple, to_s, _Args, _Block) ->
   List = tuple_to_list(Tuple),
   lists:flatten(["(", string:join([convert(Elem) || Elem <- List], ","), ")"]).
