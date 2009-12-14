fdec3ef46bd73794e50d85f154926dffd02b41a7
Tony Arcieri <tony@medioh.com>
2009-12-13 20:11:35 -0700

    Implement List#[]=

--- src/builtins/List.erl
+++ src/builtins/List.erl
@@ -7,10 +7,26 @@ call(List, '[]', {Index}, _Block) ->
     true      -> lists:nth(Index + 1, List)
   end;
   
+call(List, '[]=', {Index, Value}, _Block) ->
+  if
+    Index < 0 -> replace(List, length(List) + Index, Value);
+    true      -> replace(List, Index, Value)
+  end;
+  
 call(List, size, _Args, _Block) ->
   length(List);
   
 call(List, to_s, _Args, _Block) ->
   lists:flatten(["[", string:join([convert(Elem) || Elem <- List], ","), "]"]).
+
+replace(List, Index, Value) ->
+  replace(List, 0, Index, Value).
+
+replace([], _, _, _) ->
+  throw('bad argument');
+replace([_|Tail], Index, Index, Value) ->
+  [Value|Tail];
+replace([Head|Tail], N, Index, Value) ->
+  [Head|replace(Tail, N + 1, Index, Value)].
 
 convert(Value) -> reia_dispatch:call(Value, to_s, [], nil).
