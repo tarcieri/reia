ed292ff0aad8383e5692469e00af237b94574fd5
Tony Arcieri <tony@medioh.com>
2009-12-19 12:14:32 -0700

    Initial core type definitions

---
+++ src/compiler/reia_types.hrl
@@ -0,0 +1,5 @@
+% Records for Reia core types which are not native to Erlang
+% These are hopefully temporary and will be replaced by immutable objects
+
+-record(reia_string, {members = []}).
+-record(reia_regex,  {pattern}).
