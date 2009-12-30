23b6d7e4f4e87965b26b19c7928dd7cff281a6b7
Tony Arcieri <tony@medioh.com>
2009-12-29 21:41:30 -0700

    Ensure modules are loaded before allowing them to be inspected

--- src/builtins/Module.erl
+++ src/builtins/Module.erl
@@ -11,7 +11,13 @@
 -include("../core/reia_types.hrl").
 
 call(Module, inspect, _Args, _Block) ->
-  ?invoke(atom_to_list(Module#reia_module.name), to_string, {}, nil);
+  Name = Module#reia_module.name,
+  case code:ensure_loaded(Name) of
+    {module, Name} ->
+      ?invoke(atom_to_list(Name), to_string, {}, nil);
+    {error, _Error} ->
+      throw({error, {Name, "not loaded"}})
+  end;
   
 call(Module, to_s, Args, Block) ->
   call(Module, inspect, Args, Block).
