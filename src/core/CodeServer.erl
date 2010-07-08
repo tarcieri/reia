2d5ba2c9f4bef51d8f99e1a12a382b9530dece96
Tony Arcieri <tony@medioh.com>
2010-07-08 00:49:52 -0600

    CodeServer:call/2 thunk (Reia call semantics) and additional CodeServer debugging info

--- src/core/CodeServer.erl
+++ src/core/CodeServer.erl
@@ -7,9 +7,11 @@
 
 -module('CodeServer'). 
 -behaviour(gen_server).
+-record(state, {debug=true}).
+-define(DEBUG(Msg, Args), if State#state.debug -> io:format(Msg, Args); true -> void end).
 -export([
   % Public API
-  start/0, 
+  start/0, call/2,
   
   % gen_server callbacks
   init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3
@@ -17,21 +19,29 @@
 
 start() ->
   gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
- 
-init([]) ->
-  {ok, []}.
   
-handle_call(_Request, _From, State) ->
+call(Request, _Block) ->
+  gen_server:call('CodeServer', Request).
+  
+init([]) ->
+  {ok, #state{}}.
+  
+handle_call(Request, _From, State) ->
+  ?DEBUG("*** CodeServer: Handling call: ~p~n", [Request]),
   {reply, ok, State}.
   
-handle_cast(_Msg, State) ->
+handle_cast(Message, State) ->
+  ?DEBUG("*** CodeServer: Handling cast: ~p~n", [Message]),
   {noreply, State}.
   
-handle_info(_Info, State) ->
+handle_info(Info, State) ->
+  ?DEBUG("*** CodeServer: Handling info: ~p~n", [Info]),
   {noreply, State}.
   
-terminate(_Reason, _State) ->
+terminate(Reason, State) ->
+  ?DEBUG("*** CodeServer: Terminating: ~p~n", [Reason]),
   ok.
   
 code_change(_OldVsn, State, _Extra) ->
+  ?DEBUG("*** CodeServer: Code change~n", []),
   {ok, State}.
